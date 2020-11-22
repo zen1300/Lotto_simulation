9bb6a2c7d22305ec69c6c02b5fc4becc616da727
Zak Nolte <zen1301@gmail.com>
2020-11-22 12:58:04 -0700

    remove main

--- main.py
+++
@@ -1,8 +0,0 @@
-from simulator import Lotto
-
-
-if __name__ == '__main__':
-    sim = Lotto()
-    sim.run(jackpot=250_000_000, add_powerplay=True, num_plays=100_000)
-    print(sum(sim.winnings))
-    sim.view_results()
