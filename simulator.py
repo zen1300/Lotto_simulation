ebb1a97f66f492da89917f107e323ce9aa58c426
Zak Nolte <zen1301@gmail.com>
2020-11-25 23:15:11 -0700

    add sql export & resetting

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3
 
 import numpy as np
 import pandas as pd
@@ -20,12 +21,24 @@ class Lotto:
         sim.run()
 
     After running, the Lotto object will keep a running total of various properties of each simulation. These results
-    can be exported to a .csv file or viewed in a subplot:
+    can be exported to a .csv file, to a sql database, or viewed in a subplot:
 
     Ex:
-        sim.export_results()
+        sim.to_csv('Results.csv')
+
+        sim.to_sql('Results')
 
         sim.view_results()
+
+    Multiple successive runs can be run by aggregating results between runs or by resetting the state of results between
+    each run.
+
+    Ex:
+        sim.run(reset=False) -> Results = Run #1
+
+        sim.run(reset=False) -> Results = Run #1 + Run #2
+
+        sim.run(reset=True) -> Results = Run #3 only
     """
 
     def __init__(self):
@@ -44,18 +57,7 @@ class Lotto:
             '5 + no powerball': 1_000_000,
             '5 + powerball': 'jackpot'
         }
-        # Keep internal results of every consecutive play
-        self.plays = 0
-        self.spent = []
-        self.winnings = []
-        self.net_profit = 0
-        self.chosen_ball1 = []
-        self.chosen_ball2 = []
-        self.chosen_ball3 = []
-        self.chosen_ball4 = []
-        self.chosen_ball5 = []
-        self.chosen_powerball = []
-        self.chosen_powerplay = []
+        self._init_vals()
 
     @staticmethod
     def _create_histogram(x, **kwargs):
@@ -202,26 +204,47 @@ class Lotto:
         Convert and return lotto results in form of pandas DataFrame.
 
         Columns:
-            Ticket Number, Money Spent, Money Won, Ball 1, Ball 2, Ball 3, Ball 4, Ball 5, Powerball, Powerplay
+            Ticket Number, Money_Spent, Money_Won, Ball_1, Ball_2, Ball_3, Ball_4, Ball_5, Powerball, Powerplay,
+            Powerplay_Active, Jackpot
         :return: Pandas DataFrame containing results for each play.
         :rtype: pandas.DataFrame
         """
         data = {
-            'Ticket Number': [i for i in range(1, self.plays + 1)],
-            'Money Spent': self.spent,
-            'Money Won': self.winnings,
-            'Ball 1': self.chosen_ball1,
-            'Ball 2': self.chosen_ball2,
-            'Ball 3': self.chosen_ball3,
-            'Ball 4': self.chosen_ball4,
-            'Ball 5': self.chosen_ball5,
+            'Ticket_Number': [i for i in range(1, self.plays + 1)],
+            'Money_Spent': self.spent,
+            'Money_Won': self.winnings,
+            'Ball_1': self.chosen_ball1,
+            'Ball_2': self.chosen_ball2,
+            'Ball_3': self.chosen_ball3,
+            'Ball_4': self.chosen_ball4,
+            'Ball_5': self.chosen_ball5,
             'Powerball': self.chosen_powerball,
-            'Powerplay': self.chosen_powerplay
+            'Powerplay': self.chosen_powerplay,
+            'Powerplay_Active': [self.add_powerplay for i in range(self.plays)],
+            'Jackpot': [self.jackpot for i in range(self.plays)],
         }
         df = pd.DataFrame(data)
         return df
 
-    def run(self, jackpot=150_000_000, add_powerplay=False, num_plays=5000):
+    def _init_vals(self):
+        """
+        Initialize and keep internal results of every consecutive play.
+        """
+        self.plays = 0
+        self.spent = []
+        self.winnings = []
+        self.net_profit = 0
+        self.chosen_ball1 = []
+        self.chosen_ball2 = []
+        self.chosen_ball3 = []
+        self.chosen_ball4 = []
+        self.chosen_ball5 = []
+        self.chosen_powerball = []
+        self.chosen_powerplay = []
+        self.add_powerplay = False
+        self.jackpot = 1_000_000
+
+    def run(self, jackpot=150_000_000, add_powerplay=False, num_plays=5000, reset=False):
         """
         Run a simulated powerball drawing. Simulates a unique drawing for every play in num_plays.
         For every play, will update the following properties with the results:
@@ -237,7 +260,13 @@ class Lotto:
         :type add_powerplay: bool
         :param num_plays: Number of drawings to simulate.
         :type num_plays: int
+        :param reset: Whether to reset all results or keep an accumulation of results between runs.
+        :type reset: bool
         """
+        if reset:
+            self._init_vals()
+        self.add_powerplay = add_powerplay
+        self.jackpot = jackpot
         for play in range(num_plays):
             # update internal properties
             self.plays += 1
@@ -272,7 +301,7 @@ class Lotto:
             self.winnings.append(winnings)
         self.net_profit = np.sum(self._calc_profit())
 
-    def export_results(self, file_path):
+    def to_csv(self, file_path):
         """
         Report the results for each individual play to a .csv file.
         :param file_path: File path of output file.
@@ -280,6 +309,24 @@ class Lotto:
         """
         df = self._get_df()
         df.to_csv(file_path, index=False)
+
+    def to_sql(self, file_path, **kwargs):
+        """
+        Export the results for each individual play to an sqlite database. Compiles simulated results into a DataFrame
+        then writes results to a database. Accepts any kwargs for DataFrame.to_sql(). If a table name is not provided,
+        will default to 'Results'.
+        :param file_path: File path of database.
+        :type file_path: str
+        :param kwargs: Any acceptable DataFrame.to_sql() kwargs
+        :type kwargs: Any
+        """
+        try:
+            name = kwargs.pop('name')
+        except KeyError:
+            name = "Results"
+        conn = sqlite3.connect(file_path)
+        df = self._get_df()
+        df.to_sql(name=name, con=conn, **kwargs)
 
     def view_results(self, max_points=5_000):
         """
@@ -367,5 +414,6 @@ class Lotto:
 
 if __name__ == '__main__':
     sim = Lotto()
-    sim.run(jackpot=250_000_000, add_powerplay=True, num_plays=1_000_000)
-    sim.export_results('results.csv')
+    sim.run(jackpot=250_000_000, add_powerplay=True, num_plays=1_000_000, reset=True)
+    sim.to_csv('results.csv')
+    sim.to_sql('Lotto.db', index=False, if_exists='append')
